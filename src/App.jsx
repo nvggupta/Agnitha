a69b3d10a3efc7f32193308dd72f94440675a14e
nvggupta <ng969028@gmail.com>
2024-11-10 13:51:45 +0530

    private Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { Route, Routes } from "react-router-dom";
-import Header from "./Components/HEader";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
 import LandingPage from "./Components/LandingPage";
 import Favourite from "./Components/Favourite";
 import Books from "./Components/Books";
+import { useSelector } from "react-redux";
 
 function App() {
+  const payloadData = useSelector((state) => state.booksStore[0]);
+  const { query } = payloadData;
+
   return (
     <>
       <div>
         <Header />
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/Books" element={<Books  />} />
+          <Route
+            path="/Books"
+            element={query ? <Books /> : <Navigate to="/" replace />}
+          />
           <Route path="/Favourite" element={<Favourite />} />
         </Routes>
       </div>
